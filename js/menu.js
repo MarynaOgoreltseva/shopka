63636784ed7e7966f014c93a3b07870b6306c176
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-05-26 19:58:50 +0300

    fix js.menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,11 @@
-$(document).ready(function() {
-    $('.aside-nav .aside-nav__submenu').hide();
-    $('.aside-nav .aside-nav__item-link').on('click', accordion);
-});
+$(document).ready(function () {
+	$('.aside-nav .aside-nav__submenu').hide();
+	$('.aside-nav__subitems .aside-nav__tel').hide();
+	$('.aside-nav__submenu .aside-nav__subitems').hide();
+	$('.aside-nav .aside-nav__item-link').on('click', accordion);
+	$('.aside-nav .aside-nav__subitems-link').on('click', accordion);
 
-$(document).ready(function() {
-    $('.aside-nav__submenu .aside-nav__subitems').hide();
-    $('.aside-nav .aside-nav__item-link').on('click', accordion);
-});
-
-function accordion(){
+	function accordion(){
     $(this).next().slideToggle(1000);
 }
+})
