a34a0a9773aa9dd5e746ddc0445a54b87e59c286
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-05-30 10:32:12 +0300

    fix aside checkboxes

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,18 @@ $(document).ready(function () {
 	$('.aside-nav .aside-nav__item-link').on('click', accordion);
 	$('.aside-nav .aside-nav__subitems-link').on('click', accordion);
 
+	// $('.aside-nav').click(function() {
+    //     $('.icon_chevron-right').toggleClass('active');
+    // });
+
+	const list = document.querySelectorAll('.aside-nav li a .icon_chevron-right')
+ list.forEach(item =>{ 
+        item.addEventListener('click', (e) =>{
+        list.forEach(el=>{ el.classList.remove('icon_chevron-active'); });
+        item.classList.add('icon_chevron-active')
+    })
+})
+
 	function accordion(){
     $(this).next().slideToggle(1000);
 }
