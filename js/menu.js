b1f2f2e6ab532b4472b287e4a72e6bf3a79c54e8
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-06-06 12:02:53 +0300

    fix aside checking, features, width aside

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,16 @@
 $(document).ready(function () {
+	$('.aside-nav').hide();
 	$('.aside-nav .aside-nav__submenu').hide();
 	$('.aside-nav__subitems .aside-nav__tel').hide();
 	$('.aside-nav__submenu .aside-nav__subitems').hide();
+
+	$('.aside-checking__list').hide();
+	$('.aside-checking__title').on('click', accordion);
+
+
+	$('.btn-link_aside-btn-link').on('click', accordion);
 	$('.aside-nav .aside-nav__item-link').on('click', accordion);
 	$('.aside-nav .aside-nav__subitems-link').on('click', accordion);
-
-	// $('.aside-nav').click(function() {
-    //     $('.icon_chevron-right').toggleClass('active');
-    // });
-
-	const list = document.querySelectorAll('.aside-nav li a .icon_chevron-right')
- list.forEach(item =>{ 
-        item.addEventListener('click', (e) =>{
-        list.forEach(el=>{ el.classList.remove('icon_chevron-active'); });
-        item.classList.add('icon_chevron-active')
-    })
-})
 
 	function accordion(){
     $(this).next().slideToggle(1000);
